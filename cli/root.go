32dd1b54f715eacec2b5372d314d02e4cd4c62b8
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-04-09 19:18:51 +0800

    feat: support writing message to server (#1)
    * feat: support writing message to server
    
    * feat: support to interactive with the websocket server
    
    ---------
    
    Co-authored-by: rick <LinuxSuRen@users.noreply.github.com>

--- cli/root.go
+++ cli/root.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"bufio"
+	"fmt"
 	"net/http"
 	"net/url"
 	"os"
@@ -32,9 +34,12 @@ func (o *rootOption) runE(cmd *cobra.Command, args []string) (err error) {
 	service := args[0]
 	server := url.URL{Scheme: "ws", Host: o.server, Path: service}
 
+	header := http.Header{}
+	header.Set("Cookie", "oAuthLoginInfo=; token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODA2MTE1OTYsImlhdCI6MTY4MDYwNDM5NiwiaXNzIjoia3ViZXNwaGVyZSIsInN1YiI6ImFkbWluIiwidG9rZW5fdHlwZSI6ImFjY2Vzc190b2tlbiIsInVzZXJuYW1lIjoiYWRtaW4ifQ.kY7jpe4If37_9Kz_U9R5s6q2jIwzZ7tTaYI6KbXee_Y; expire=1680611597080; refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODA2MTg3OTYsImlhdCI6MTY4MDYwNDM5NiwiaXNzIjoia3ViZXNwaGVyZSIsInN1YiI6ImFkbWluIiwidG9rZW5fdHlwZSI6InJlZnJlc2hfdG9rZW4iLCJ1c2VybmFtZSI6ImFkbWluIn0.8niwD-WQK7LMDb-T9B177v0rVpWWP21F7qc8ve2qNrc; lang=zh")
+
 	var conn *websocket.Conn
 	var resp *http.Response
-	if conn, resp, err = websocket.DefaultDialer.Dial(server.String(), nil); err != nil {
+	if conn, resp, err = websocket.DefaultDialer.Dial(server.String(), header); err != nil {
 		cmd.Println("failed to dial, response is", resp)
 		return
 	}
@@ -44,6 +49,17 @@ func (o *rootOption) runE(cmd *cobra.Command, args []string) (err error) {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
+	conn.WriteMessage(websocket.TextMessage, []byte("\n"))
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			data := []byte(scanner.Text() + "\n")
+			if err := conn.WriteMessage(websocket.TextMessage, data); err != nil {
+				fmt.Println("failed to send message", err)
+			}
+		}
+	}()
+
 	go func() {
 		defer close(done)
 		for {
@@ -51,7 +67,7 @@ func (o *rootOption) runE(cmd *cobra.Command, args []string) (err error) {
 				err = msgErr
 				return
 			} else {
-				cmd.Println(string(message))
+				cmd.Print(string(message))
 			}
 		}
 	}()

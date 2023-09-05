1c3eb845bc39ef890c3250a8f5dba5605c4b390b
rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-09-05 14:53:36 +0800

    add gorelease for releasing

--- cli/root.go
+++ cli/root.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
 
@@ -15,31 +14,38 @@ import (
 func NewRootCmd() (cmd *cobra.Command) {
 	opt := &rootOption{}
 	cmd = &cobra.Command{
-		Use:   "gows",
-		Short: "Echo the message from a websoket server",
-		Args:  cobra.MinimumNArgs(1),
-		RunE:  opt.runE,
+		Use:     "gows",
+		Example: "gwws ws://your-server.com",
+		Short:   "Echo the message from a websoket server",
+		Args:    cobra.MinimumNArgs(1),
+		PreRunE: opt.preRunE,
+		RunE:    opt.runE,
 	}
 	flags := cmd.Flags()
-	flags.StringVarP(&opt.server, "server", "s", "", "The server address")
-	cmd.MarkFlagRequired("server")
+	flags.StringVarP(&opt.cookie, "cookie", "", "", "The cookie to connect to the server")
 	return
 }
 
 type rootOption struct {
-	server string
+	cookie string
+}
+
+func (o *rootOption) preRunE(cmd *cobra.Command, args []string) (err error) {
+	o.cookie = os.ExpandEnv(o.cookie)
+	return
 }
 
 func (o *rootOption) runE(cmd *cobra.Command, args []string) (err error) {
 	service := args[0]
-	server := url.URL{Scheme: "ws", Host: o.server, Path: service}
 
 	header := http.Header{}
-	header.Set("Cookie", "oAuthLoginInfo=; token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODA2MTE1OTYsImlhdCI6MTY4MDYwNDM5NiwiaXNzIjoia3ViZXNwaGVyZSIsInN1YiI6ImFkbWluIiwidG9rZW5fdHlwZSI6ImFjY2Vzc190b2tlbiIsInVzZXJuYW1lIjoiYWRtaW4ifQ.kY7jpe4If37_9Kz_U9R5s6q2jIwzZ7tTaYI6KbXee_Y; expire=1680611597080; refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODA2MTg3OTYsImlhdCI6MTY4MDYwNDM5NiwiaXNzIjoia3ViZXNwaGVyZSIsInN1YiI6ImFkbWluIiwidG9rZW5fdHlwZSI6InJlZnJlc2hfdG9rZW4iLCJ1c2VybmFtZSI6ImFkbWluIn0.8niwD-WQK7LMDb-T9B177v0rVpWWP21F7qc8ve2qNrc; lang=zh")
+	if o.cookie != "" {
+		header.Set("Cookie", o.cookie)
+	}
 
 	var conn *websocket.Conn
 	var resp *http.Response
-	if conn, resp, err = websocket.DefaultDialer.Dial(server.String(), header); err != nil {
+	if conn, resp, err = websocket.DefaultDialer.Dial(service, header); err != nil {
 		cmd.Println("failed to dial, response is", resp)
 		return
 	}
